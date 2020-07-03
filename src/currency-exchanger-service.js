89a86d7c7211e76ca1be4bb2d83690a7f8a568a9
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-03 07:48:09 -0700

    write separated business logic for API query.

---
+++ src/currency-exchanger-service.js
@@ -0,0 +1,18 @@
+export class ExchangeService { 
+  async getExchangeRates() {
+    try {
+      let response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if(response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      console.log(jsonifiedResponse)
+      return jsonifiedResponse;
+    } catch(error) {
+      console.log(error)
+      return false;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { ExchangeService } from './currency-exchanger-service'
 
-$(document).ready(function () {
+$(document).ready(function () {})
